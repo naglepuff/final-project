a13ce3c7fd6f77fbbfbf6a85b5782ad0a5423dbb
Michael Nagler <m.nagler995@gmail.com>
2017-06-16 12:29:44 -0400

    added navbar and some bootstrap css from bootswatch.com

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+import {Component} from 'angular2/core';
+import {NavBarComponent} from './navbar.component';
+
+@Component({
+    selector: 'my-app',
+    template: `
+        <navbar></navbar>
+    `,
+    directives: [NavBarComponent]
+})
+export class AppComponent { }
